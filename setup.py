fb5d995e2783d94fc4759ba6a6283d0a49c84ab8
marcosfelt <kcfelton@ncsu.edu>
2019-03-15 13:20:50 +0000

    Explicitly specify dependencies (i.e., pyserial)

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from commanduino._version import __version__
 
 from setuptools import find_packages, setup
+
+requirements = ['pyserial']
+
 setup(name="commanduino",
       version=__version__,
       description="A library to interact with an arduino board running Arduino-CommandHandler and derivatives",
@@ -8,5 +11,5 @@ setup(name="commanduino",
       author_email='jonathan.grizou@gmail.com',
       platforms=["any"],
       url="https://github.com/croningp/commanduino",
-      packages=find_packages(),
+      packages=requirements,
       )

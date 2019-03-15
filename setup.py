21babf4c11b86213b4a1045c1b14d965ed0c3afe
marcosfelt <kcfelton@ncsu.edu>
2019-03-15 13:26:44 +0000

    Actually fix the dependency problem

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
-from commanduino._version import __version__
+# from commanduino._version import __version__
 
 from setuptools import find_packages, setup
 
 requirements = ['pyserial']
 
 setup(name="commanduino",
-      version=__version__,
+      version='0.1',
       description="A library to interact with an arduino board running Arduino-CommandHandler and derivatives",
       author="Jonathan Grizou",
       author_email='jonathan.grizou@gmail.com',
       platforms=["any"],
       url="https://github.com/croningp/commanduino",
-      packages=requirements,
+      install_requires=requirements,
+      packages=find_packages(),
       )
